d34ce7496b39481957d5abc63b52aad0f8d1b9fe
marine khloyan <Marine.Khloyan@Digitain.com>
2018-11-19 17:51:16 +0400

    adding dummy text

--- src/components/QuestionPage/QuestionsCont/QuestionsCont.js
+++ src/components/QuestionPage/QuestionsCont/QuestionsCont.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './QuestionsCont.scss';
+import QuestionItem from './QuestionItem/QuestionItem';
 
 export default class QuestionsCont extends Component {
     render() {
         return (
             <div className='am--questions-container'>
-
+                <QuestionItem />
+                <QuestionItem />
             </div>
         )
     }
--- src/components/QuestionPage/QuestionsFilter/QuestionsFilter.js
+++ src/components/QuestionPage/QuestionsFilter/QuestionsFilter.js
@@ -4,8 +4,20 @@ import './QuestionsFilter.scss';
 export default class QuestionsFilter extends Component {
     render() {
         return (
-            <div className='am--questions-filter-container'>
-                Filter side
+            <div className='am--questions-filter-container '>
+                <ul className='am--flex am--flex-column am--align-start'>
+                    <li>All</li>
+                    <li className='am-flex am--flex-column am--align-start'>
+                        Skill
+                        <ul className='am-flex am--flex-column am--align-start'>
+                            <li>Doctor</li>
+                            <li>Programmer</li>
+                        </ul>
+                    </li>
+                    <li>Age</li>
+                    <li>Gender</li>
+                    <li>Answer Later</li>
+                </ul>
             </div>
         )
     }
