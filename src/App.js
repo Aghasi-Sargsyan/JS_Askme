fef2ab3262e79b647a5b21833a63324c67d55e11
marine khloyan <Marine.Khloyan@Digitain.com>
2018-11-19 16:50:46 +0400

    fixing registration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { auth } from "firebase";
 import SignIn from "./components/registration/SingIn/SignIn";
 import SignUp from "./components/registration/SignUp/SignUp";
@@ -8,44 +8,21 @@ import QuestionPage from "./components/QuestionPage/QuestionPage";
 import Main from "./components/Main/Main";
 import AuthorizedRoute from "./components/registration/AuthorizedRoute";
 import RegContainer from "./components/registration/RegContainer";
+
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-
-
-    this.state = {
-      user: {}
-    };
-  }
-
-  componentDidMount() {
-    this.authListener();
-  }
-
-  authListener() {
-    auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({ user });
-      } else {
-        this.setState({ user: null });
-      }
-    });
-  }
-
   render() {
     return (
       <Router>
         <div>
-          <Main />
           <RegContainer />
           <hr />
-          <Route exact path="/" component={SignIn} />
+          <Route exact path="/" component={Main} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/signin" component={SignIn} />
           <Route exact path="/profile" component={Profile} />
-          <Route exact path="/questions" component={QuestionPage} />
+          <Switch>
+            <AuthorizedRoute exact path="/questions" component={QuestionPage} />
+          </Switch>
         </div>
       </Router>
     );
--- src/components/registration/AuthorizedRoute.js
+++ src/components/registration/AuthorizedRoute.js
@@ -7,41 +7,56 @@ export default class AuthorizedRoute extends Component {
     super(props);
 
     this.state = {
-      authUser: false,
-      pending: true
+      user: {},
     };
   }
 
   componentDidMount() {
     auth().onAuthStateChanged(user => {
-      this.setState({
-        pending: false,
-        authUser: !!user
-      });
-      // authUser
-      //   ? this.setState({ authUser })
-      //   : this.setState({ authUser: null });
-    });
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    // this.setState({
+    //   user: null
+    // })
+    // auth().onAuthStateChanged(user => {
+    //   if (user) {
+    //     this.setState({ user: null });
+    //   }
+    // })
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    })
   }
 
   render() {
     const { component: Component, ...rest } = this.props;
-    const { authUser, pending } = this.state;
+    const { user } = this.state;
     return (
       <Route
         {...rest}
         render={renderProps => {
-          if (pending) return null;
-          return authUser ? (
+          // if (pending) return null;
+          return user ? (
             <Component {...renderProps} />
           ) : (
-            <Redirect
-              to={{
-                pathname: "/signin",
-                state: { from: renderProps.location }
-              }}
-            />
-          );
+              <Redirect
+                to={{
+                  pathname: "/signin",
+                  state: { from: renderProps.location }
+                }}
+              />
+            );
         }}
       />
     );
