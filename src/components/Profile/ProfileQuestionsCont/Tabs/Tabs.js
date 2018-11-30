bd827392a0bf6cf9fb46d121821b0f3f93f7633e
Karo Zakaryan <karo.zakaryan.y@tumo.org>
2018-11-30 21:23:49 +0400

    Tabs,Pane...

---
+++ src/components/Profile/ProfileQuestionsCont/Tabs/Tabs.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Tabs extends Component {
+    constructor(props) {
+      super(props);  
+      
+      this.state = {
+        selected: props.selected
+      }
+    }
+
+    clickHandler = (index, e) => {
+        e.preventDefault();
+        this.setState({
+          selected: index
+        });
+    }
+    
+    rendTabLabels() {
+      const labels = (child, index) => {
+        let activeClass = (this.state.selected === index ? 'active' : '');
+        return (
+          <li key={index}>
+            <a className={activeClass}  onClick={(e) => this.clickHandler(index, e)} href="#">
+              {child.props.label}
+            </a>
+          </li>
+        );
+      }
+    
+     return (
+        <ul className="tabs__labels">
+          {this.props.children.map(labels)}
+        </ul>
+      );
+    }
+    
+    render() {
+      return (
+        <div className="tabs">
+            {this.rendTabLabels()}
+            
+            <div className="tabs__content">
+                {this.props.children[this.state.selected]}
+            </div>
+        </div>
+        );
+    }
+}
+
+export default Tabs;
