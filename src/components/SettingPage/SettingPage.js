cb0f1ab7cc1c1f3236ade5e929b847d1c43271ee
Karo Zakaryan <karo.zakaryan.y@tumo.org>
2018-12-01 13:55:36 -0800

    Setting page progress...

--- src/components/SettingPage/SettingPage.js
+++ src/components/SettingPage/SettingPage.js
@@ -1,67 +1,107 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import "./SettingPage.scss";
 import Input from "../universal/Input/Input";
+import male from "../../assets/icons/male.png";
+import female from "../../assets/icons/female.png";
 
 class SettingPage extends Component {
 
     state = {
-        user: {
-            userName: "John Doe",
-            birthYear: {
-                value: "1900",
-                maxYear: new Date().getFullYear() - 14,
-                valid: true,
-                errorMessage: "invalid age",
-                id: "birthYear"
-            },
-            gender: "male",
-            email: "test@mail.ru",
-            password: "123456"
+        userName: "John Doe",
+        birthYear: "1900",
+        gender: "Male",
+        email: "test@mail.ru",
+        password: "123456"
+    };
+
+    handleChange = e => {
+        this.setState({[e.target.id]: e.target.value});
+    };
+
+    handleClick = e => {
+        e.preventDefault();
+        const currentElem = e.target.id.substr(1);
+        if (this.state[currentElem].length) {
+            this.setState({userName: this.state.userName});
+        } else {
+            this.setState({userName: this.state.userName});
+            alert("length is zero!");
         }
-    }
+    };
+
+    handleCheck = (e) => {
+        this.setState({
+            gender: e.target.value
+        });
+    };
 
     render() {
-        const {userName, birthYear, gender, email, password} = this.state.user;
+        const {userName, birthYear, gender, email, password} = this.state;
         return (
             <div className="setting__page">
                 <div className="setting__profile">
                     <h1>{userName}</h1>
-                    <h2>Username</h2>
-                    <p>
-                        <Input type="text" value={userName}/>
-                        <button className="btn">
-                            Update
-                        </button>
-                    </p>
-                    <h2>Birth Year</h2>
-                    <p>
-                        <Input
-                            type="number"
-                            id={birthYear.id}
-                            value={birthYear.value}
-                            valid={birthYear.valid}
-                            changeHandler={this.handleChange}
-                            errorMessage={birthYear.errorMessage}
-                        />
-                    </p>
-                    <h2>Gender</h2>
-                    <p>
-                        {gender}
-                    </p>
-                    <h2>Email</h2>
-                    <p>
-                        <Input type="email" value={email}/>
-                        <button className="btn">
-                            update
-                        </button>
-                    </p>
-                    <h2>Password </h2>
-                    <p>
-                        <Input type="password" value={password}/>
-                        <button className="btn">
-                            Change
-                        </button>
-                    </p>
+                    <form className="flex flex_column align_center">
+                        <div>
+                            <h2>Username</h2>
+                            <Input type="text" id="userName" value={userName} changeHandler={this.handleChange}/>
+                            <button id="_userName" className="btn" onClick={this.handleClick}>
+                                Update
+                            </button>
+                        </div>
+                        <hr/>
+                        <div>
+                            <h2>Birth Year</h2>
+                            <Input
+                                type="number"
+                                id="birthYear"
+                                value={birthYear}
+                                changeHandler={this.handleChange}
+                            />
+                        </div>
+                        <hr/>
+                        <div>
+                            <h2>Gender</h2>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="genderGroup"
+                                    id="radioMale"
+                                    defaultChecked="true"
+                                    value="Male"
+                                    onChange={this.handleCheck}
+                                />
+                                <img src={male} alt="male"/>
+                            </label>
+                            <label>
+                                <input type="radio"
+                                       name="genderGroup"
+                                       value="Female"
+                                       id="radioFemale"
+                                       onChange={this.handleCheck}
+                                />
+                                <img src={female} alt="female"/>
+
+                            </label>
+                        </div>
+                        <div>
+                            <h2>Email</h2>
+                            <Input type="email" id="email" value={email} changeHandler={this.handleChange}/>
+                            <button id="_email" className="btn" onClick={this.handleClick}>
+                                update
+                            </button>
+                        </div>
+                        <hr/>
+                        <div>
+                            <h2>Password </h2>
+                            <Input type="password" id="password" value={password} changeHandler={this.handleChange}/>
+                            <button id="_password" className="btn" onClick={this.handleClick}>
+                                Change
+                            </button>
+                        </div>
+                        <hr/>
+                        <button type="button" className="settings__save__btn">Save Changes</button>
+                    </form>
                 </div>
             </div>
         );
