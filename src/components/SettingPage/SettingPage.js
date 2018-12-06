3b9457b2486838652045a299f0a0e6f19fb0b93c
MarineKh <admin@Marine.local>
2018-12-06 08:57:17 +0400

    setting styling

--- src/components/SettingPage/SettingPage.js
+++ src/components/SettingPage/SettingPage.js
@@ -1,8 +1,9 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./SettingPage.scss";
 import Input from "../universal/Input/Input";
 import male from "../../assets/icons/male.png";
 import female from "../../assets/icons/female.png";
+import settingIcon from "../../assets/icons/settings.png";
 
 class SettingPage extends Component {
 
@@ -11,20 +12,21 @@ class SettingPage extends Component {
         birthYear: "1900",
         gender: "Male",
         email: "test@mail.ru",
-        password: "123456"
+        password: "123456",
+        confPassword: '123456'
     };
 
     handleChange = e => {
-        this.setState({[e.target.id]: e.target.value});
+        this.setState({ [e.target.id]: e.target.value });
     };
 
     handleClick = e => {
         e.preventDefault();
         const currentElem = e.target.id.substr(1);
         if (this.state[currentElem].length) {
-            this.setState({userName: this.state.userName});
+            this.setState({ userName: this.state.userName });
         } else {
-            this.setState({userName: this.state.userName});
+            this.setState({ userName: this.state.userName });
             alert("length is zero!");
         }
     };
@@ -36,32 +38,41 @@ class SettingPage extends Component {
     };
 
     render() {
-        const {userName, birthYear, email, password} = this.state;
+        const { userName, birthYear, email, password } = this.state;
         return (
             <div className="setting__page">
+                <div className='setting__aside'>
+                    <div className='flex  align_center'>
+                        <img className='setting-icon' src={settingIcon} alt='setting' />
+                        <p>Settings</p>
+                    </div>
+                </div>
                 <div className="setting__profile">
                     <h1>{userName}</h1>
-                    <form className="flex flex_column align_center">
+                    <form className="flex flex_column">
                         <div>
-                            <h2>Username</h2>
-                            <Input type="text" id="userName" value={userName} changeHandler={this.handleChange}/>
+                            <div className='flex align_center'>
+                                <label>Username </label>
+                                <Input type="text" valid id="userName" value={userName} changeHandler={this.handleChange} />
+                            </div>
                             <button id="_userName" className="btn" onClick={this.handleClick}>
                                 Update
                             </button>
                         </div>
-                        <hr/>
-                        <div>
-                            <h2>Birth Year</h2>
+                        <hr />
+                        <div className='flex align_center'>
+                            <label>Birth Year</label>
                             <Input
                                 type="number"
                                 id="birthYear"
+                                valid
                                 value={birthYear}
                                 changeHandler={this.handleChange}
                             />
                         </div>
-                        <hr/>
-                        <div>
-                            <h2>Gender</h2>
+                        <hr />
+                        <div className='setting__page_gender'>
+                            <span className='pad_right_20'>Gender </span>
                             <label>
                                 <input
                                     type="radio"
@@ -71,35 +82,49 @@ class SettingPage extends Component {
                                     value="Male"
                                     onChange={this.handleCheck}
                                 />
-                                <img src={male} alt="male"/>
+                                <img src={male} alt="male" />
                             </label>
                             <label>
                                 <input type="radio"
-                                       name="genderGroup"
-                                       value="Female"
-                                       id="radioFemale"
-                                       onChange={this.handleCheck}
+                                    name="genderGroup"
+                                    value="Female"
+                                    id="radioFemale"
+                                    onChange={this.handleCheck}
                                 />
-                                <img src={female} alt="female"/>
-
+                                <img className='female' src={female} alt="female" />
                             </label>
                         </div>
+                        <hr />
                         <div>
-                            <h2>Email</h2>
-                            <Input type="email" id="email" value={email} changeHandler={this.handleChange}/>
+                            <div className='flex align_center'>
+                                <label>Email </label>
+                                <Input type="email" id="email" valid value={email} changeHandler={this.handleChange} />
+                            </div>
                             <button id="_email" className="btn" onClick={this.handleClick}>
                                 update
                             </button>
                         </div>
-                        <hr/>
+                        <hr />
                         <div>
-                            <h2>Password </h2>
-                            <Input type="password" id="password" value={password} changeHandler={this.handleChange}/>
+                            <div className='flex align_center'>
+                                <label>Password </label>
+                                <Input type="password" id="password" valid value={password} changeHandler={this.handleChange} />
+                            </div>
                             <button id="_password" className="btn" onClick={this.handleClick}>
                                 Change
                             </button>
                         </div>
-                        <hr/>
+                        <hr />
+                        <div>
+                            <div className='flex align_center'>
+                                <label>Update Password </label>
+                                <Input type="password" id="confPassword" valid value={password} changeHandler={this.handleChange} />
+                            </div>
+                            <button id="_password" className="btn" onClick={this.handleClick}>
+                                Change
+                            </button>
+                        </div>
+                        <hr />
                         <button type="button" className="settings__save__btn">Save Changes</button>
                     </form>
                 </div>
